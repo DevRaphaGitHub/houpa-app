de97347d0f44c02b774f54ac4632b93e65ecc744
Raphael Cunha <raphaelcdutra93@gmail.com>
2020-11-29 14:32:49 -0300

    ...

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
+import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 
 // Barra direita -> Botões pesquisa, favoritos e carrinho de compras
 import InputBase from '@material-ui/core/InputBase';
@@ -108,39 +97,15 @@ const useStyles = makeStyles((theme) => ({
 
 const Navbar = () => {
   const classes = useStyles();
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
 
   return (
     <div className={classes.root}>
       <CssBaseline />
       <AppBar
         position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
+        className={classes.appBar}
       >
         <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, open && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            Menu
-          </Typography>
           <div className={classes.grow} />
           <Typography className={classes.logo}>
             <img src={logo} alt="logo" width="100px" />
@@ -163,42 +128,8 @@ const Navbar = () => {
           </IconButton>
         </Toolbar>
       </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="left"
-        open={open}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <main
-        className={clsx(classes.content, {
-          [classes.contentShift]: open,
-        })}
-      >
+      <main>
         <div className={classes.drawerHeader} />
-        <Typography paragraph>
-          
-        </Typography>
-        <Typography paragraph>
-
-        </Typography>
       </main>
     </div>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,70 @@
-import React from 'react';
-import './styles.css';
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
 import Navbar from '../../components/Navbar';
 
+const useStyles = makeStyles({
+  root: {
+  },
+  home: {
+    padding: 10,
+  },
+  container: {
+    width: '100%',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(4, 25%)',
+  },
+  card: {
+    maxWidth: 250,
+    margin: 10,
+  },
+  media: {
+    height: 300,
+  },
+});
+
 const Home = () => {
+  const classes = useStyles();
+  const [products, setProducts] = useState([]);
+
+  // Requisição para api PHP
+  useEffect(() => {
+    fetch("http://localhost/houpa-app/src/api/")
+      .then(response => response.json())
+      .then(
+        (result) => {
+          setProducts(result);
+        }
+      )
+  }, []);
+
   return (
-    <Navbar />
+    <div className={classes.home}>
+      <Navbar />
+      <div className={classes.container}>
+      {products.map((product) => (
+        <Card className={classes.card}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image={product.photo}
+              title={product.name}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                <span>{product.name}</span>
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      ))}
+      </div>
+    </div>
   );
 }
 
